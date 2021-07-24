b00c7949813e46c42dacb6b0cc0aa5c6f0dabe16
Dawit Aseged <43924962+Dawit-Aseged@users.noreply.github.com>
2021-07-24 23:36:20 +0300

    Side navigation outline complete
    Animations for mobile view and desktop view are done. This includes a backdrop for mobile users.
    
    Stuff Left:
    - Handle content being given to sidenav.
    - Content of Sidenav

--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -1,24 +1,45 @@
 import { CommunicationService } from './../communication.service';
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.scss']
 })
-export class SidenavComponent implements OnInit {
+export class SidenavComponent implements OnInit, OnDestroy {
   public sidenavSub: Subscription;
-  public isSidenavOpen :boolean;
-  constructor(public commService: CommunicationService) {
+  public isSidenavOpen: boolean;
+  private isMobile!: boolean
+  isHandset: Observable<BreakpointState> = this.breakpointObserver.observe(Breakpoints.Handset);
+
+
+  constructor(private breakpointObserver: BreakpointObserver, public commService: CommunicationService) {
     this.isSidenavOpen = this.commService.isSidenavOpen;
+
+    this.isHandset
+      .subscribe((state: BreakpointState) => {
+        this.isMobile = state.matches;
+    })
     this.sidenavSub = this.commService.getSidenavStatus()
       .subscribe((sidenavStat) => {
         this.isSidenavOpen = sidenavStat;
       })
-   }
+  }
 
   ngOnInit(): void {
   }
 
+  ngOnDestroy(): void {
+    this.sidenavSub.unsubscribe();
+  }
+  contentClicked(element: HTMLDivElement) {
+    if(this.isSidenavOpen && this.isMobile)
+      this.commService.toggleSidenav();
+  }
+
+  test() {
+    console.log("Test")
+  }
 }
